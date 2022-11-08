21b17dcd135d0b7a146bd5a73b6f110f13f8f851
Pilat01 <mateuszpilat012003@gmail.com>
2022-11-08 07:36:17 +0100

    1.0 ENGINE UPDATE

--- engine.js
+++ engine.js
@@ -40,56 +40,115 @@ class Game{
     constructor(){
       this.webglSETUP();
       var cube = new BodyModel();
-      cube.verts = [
-       
-      ];
+      cube.verts = new Float32Array([
+       0,0,0,
+       0,150,0,
+       150,0,0,
+       0,150,0,
+       150,150,0,
+       150,0,0
+      ]);
+      cube.colors = new Uint8Array([
+        250,0,0,
+        250,0,0,
+        0,250,0,
+        0,250,0,
+        0,0,250,
+        0,0,250
+      ]);
       cube.vertsBuffer = gl.createBuffer();
-      gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cube.vertsBuffer);
-      gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(cube.verts), gl.STATIC_DRAW);
+      gl.bindBuffer(gl.ARRAY_BUFFER, cube.vertsBuffer);
+      gl.bufferData(gl.ARRAY_BUFFER, cube.verts, gl.STATIC_DRAW);
      
       cube.colorsBuffer = gl.createBuffer();
-      gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cube.colorsBuffer);
-      gl.bufferData(gl.ELEMENT_ARRAY_BUFFER,
-        new Uint16Array(cube.colors), gl.STATIC_DRAW);
+      gl.bindBuffer(gl.ARRAY_BUFFER, cube.colorsBuffer);
+      gl.bufferData(gl.ARRAY_BUFFER, cube.colors, gl.STATIC_DRAW);
 
 
-      this.BodyModels.push();
+      this.BodyModels.push(cube);
     }
 
     webglSETUP(){
-        var GObjects = this.GameObjects;
-        var canvas = document.querySelector("#canvas");
-        gl = canvas.getContext("webgl");
-        if (!gl) {
-          return;
-        }
-      
-        // setup GLSL program
-        program = webglUtils.createProgramFromScripts(gl, ["vertex-shader-3d", "fragment-shader-3d"]);
-      
-        // look up where the vertex data needs to go.
-        positionLocation = gl.getAttribLocation(program, "a_position");
-        colorLocation = gl.getAttribLocation(program, "a_color");
-        matrixLocation = gl.getUniformLocation(program, "u_matrix");
-        gl.enable(gl.CULL_FACE);
+       
+       gl = canvas.getContext("webgl");
+      if (!gl) {
+        return;
+      }
+    
+      // setup GLSL program
+       program = webglUtils.createProgramFromScripts(gl, ["vertex-shader-3d", "fragment-shader-3d"]);
+    
+      // look up where the vertex data needs to go.
+       positionLocation = gl.getAttribLocation(program, "a_position");
+       colorLocation = gl.getAttribLocation(program, "a_color");
+    
+      // lookup uniforms
+       matrixLocation = gl.getUniformLocation(program, "u_matrix");
+    
+    }
 
-        // Enable the depth buffer
-        gl.enable(gl.DEPTH_TEST);
-        
-        gl.useProgram(program);
+    render(){
+      var translation = [45, 150, 0];
+      var rotation = [degToRad(0), degToRad(0), degToRad(0)];
+      var scale = [1, 1, 1];
+    
+  function radToDeg(r) {
+    return r * 180 / Math.PI;
+  }
 
-        // Turn on the position attribute
-        gl.enableVertexAttribArray(positionLocation);
-        gl.enableVertexAttribArray(colorLocation);
-         // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
-        var size = 3;          // 3 components per iteration
+  function degToRad(d) {
+    return d * Math.PI / 180;
+  }
+
+  var cameraAngleRadians = degToRad(0);
+  var fieldOfViewRadians = degToRad(60);
+
+
+
+
+  webglUtils.resizeCanvasToDisplaySize(gl.canvas);
+
+    // Tell WebGL how to convert from clip space to pixels
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+    // Clear the canvas AND the depth buffer.
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    // Turn on culling. By default backfacing triangles
+    // will be culled.
+    gl.enable(gl.CULL_FACE);
+
+    // Enable the depth buffer
+    gl.enable(gl.DEPTH_TEST);
+
+    // Tell it to use our program (pair of shaders)
+    gl.useProgram(program);
+
+    // Turn on the position attribute
+    gl.enableVertexAttribArray(positionLocation);
+
+    
+
+
+  
+    // Bind the position buffer.
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.BodyModels[0].vertsBuffer);
+
+    // Tell the position attribute how to get data out of positionBuffer (ARRAY_BUFFER)
+    var size = 3;          // 3 components per iteration
     var type = gl.FLOAT;   // the data is 32bit floats
     var normalize = false; // don't normalize the data
     var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
     var offset = 0;        // start at the beginning of the buffer
     gl.vertexAttribPointer(
         positionLocation, size, type, normalize, stride, offset);
-    
+
+    // Turn on the color attribute
+    gl.enableVertexAttribArray(colorLocation);
+
+    // Bind the color buffer.
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.BodyModels[0].colorsBuffer);
+
     // Tell the attribute how to get data out of colorBuffer (ARRAY_BUFFER)
     var size = 3;                 // 3 components per iteration
     var type = gl.UNSIGNED_BYTE;  // the data is 8bit unsigned values
@@ -98,78 +157,26 @@ class Game{
     var offset = 0;               // start at the beginning of the buffer
     gl.vertexAttribPointer(
         colorLocation, size, type, normalize, stride, offset);
-    }
 
-    render(){
-      webglUtils.resizeCanvasToDisplaySize(gl.canvas);
+    // Compute the matrices
+    var matrix = m4.projection(gl.canvas.clientWidth, gl.canvas.clientHeight, 400);
+    matrix = m4.translate(matrix, translation[0], translation[1], translation[2]);
+    matrix = m4.xRotate(matrix, rotation[0]);
+    matrix = m4.yRotate(matrix, rotation[1]);
+    matrix = m4.zRotate(matrix, rotation[2]);
+    matrix = m4.scale(matrix, scale[0], scale[1], scale[2]);
 
-      // Tell WebGL how to convert from clip space to pixels
-      gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+    // Set the matrix.
+    gl.uniformMatrix4fv(matrixLocation, false, matrix);
+
+    // Draw the geometry.
+    var primitiveType = gl.TRIANGLES;
+    var offset = 0;
+    var count = 16 * 6;
+    gl.drawArrays(primitiveType, offset, count);
+    
+  }
   
-      // Clear the canvas.
-      gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-      
-      // Tell it to use our program (pair of shaders)
-      gl.useProgram(program);
-      var cameraAngleRadians = degToRad(0);
-      var fieldOfViewRadians = degToRad(60);
-     
-
-    var numFs = 5;
-    var radius = 200;
-
-    // Compute the projection matrix
-    var aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
-    var zNear = 1;
-    var zFar = 2000;
-    var projectionMatrix = m4.perspective(fieldOfViewRadians, aspect, zNear, zFar);
-
-    // Compute the position of the first F
-    var fPosition = [radius, 0, 0];
-
-    // Use matrix math to compute a position on a circle where
-    // the camera is
-    var cameraMatrix = m4.yRotation(cameraAngleRadians);
-    cameraMatrix = m4.translate(cameraMatrix, 0, 0, radius * 1.5);
-
-    // Get the camera's position from the matrix we computed
-    var cameraPosition = [
-      cameraMatrix[12],
-      cameraMatrix[13],
-      cameraMatrix[14],
-    ];
-
-    var up = [0, 1, 0];
-
-    // Compute the camera's matrix using look at.
-    var cameraMatrix = m4.lookAt(cameraPosition, fPosition, up);
-
-    // Make a view matrix from the camera matrix
-    var viewMatrix = m4.inverse(cameraMatrix);
-
-    // Compute a view projection matrix
-    var viewProjectionMatrix = m4.multiply(projectionMatrix, viewMatrix);
-
-    for (var ii = 0; ii < numFs; ++ii) {
-      var angle = ii * Math.PI * 2 / numFs;
-      var x = Math.cos(angle) * radius;
-      var y = Math.sin(angle) * radius;
-
-      // starting with the view projection matrix
-      // compute a matrix for the F
-      var matrix = m4.translate(viewProjectionMatrix, x, 0, y);
-
-      // Set the matrix.
-      gl.uniformMatrix4fv(matrixLocation, false, matrix);
-
-      // Draw the geometry.
-      var primitiveType = gl.TRIANGLES;
-      var offset = 0;
-      var count = 16 * 6;
-      gl.drawArrays(primitiveType, offset, count);
-    }
-  
-    }
 }
 class Camera{
 
@@ -334,3 +341,143 @@ function radToDeg(r) {
     return d * Math.PI / 180;
   }
 
+  var m4 = {
+
+    projection: function(width, height, depth) {
+      // Note: This matrix flips the Y axis so 0 is at the top.
+      return [
+         2 / width, 0, 0, 0,
+         0, -2 / height, 0, 0,
+         0, 0, 2 / depth, 0,
+        -1, 1, 0, 1,
+      ];
+    },
+  
+    multiply: function(a, b) {
+      var a00 = a[0 * 4 + 0];
+      var a01 = a[0 * 4 + 1];
+      var a02 = a[0 * 4 + 2];
+      var a03 = a[0 * 4 + 3];
+      var a10 = a[1 * 4 + 0];
+      var a11 = a[1 * 4 + 1];
+      var a12 = a[1 * 4 + 2];
+      var a13 = a[1 * 4 + 3];
+      var a20 = a[2 * 4 + 0];
+      var a21 = a[2 * 4 + 1];
+      var a22 = a[2 * 4 + 2];
+      var a23 = a[2 * 4 + 3];
+      var a30 = a[3 * 4 + 0];
+      var a31 = a[3 * 4 + 1];
+      var a32 = a[3 * 4 + 2];
+      var a33 = a[3 * 4 + 3];
+      var b00 = b[0 * 4 + 0];
+      var b01 = b[0 * 4 + 1];
+      var b02 = b[0 * 4 + 2];
+      var b03 = b[0 * 4 + 3];
+      var b10 = b[1 * 4 + 0];
+      var b11 = b[1 * 4 + 1];
+      var b12 = b[1 * 4 + 2];
+      var b13 = b[1 * 4 + 3];
+      var b20 = b[2 * 4 + 0];
+      var b21 = b[2 * 4 + 1];
+      var b22 = b[2 * 4 + 2];
+      var b23 = b[2 * 4 + 3];
+      var b30 = b[3 * 4 + 0];
+      var b31 = b[3 * 4 + 1];
+      var b32 = b[3 * 4 + 2];
+      var b33 = b[3 * 4 + 3];
+      return [
+        b00 * a00 + b01 * a10 + b02 * a20 + b03 * a30,
+        b00 * a01 + b01 * a11 + b02 * a21 + b03 * a31,
+        b00 * a02 + b01 * a12 + b02 * a22 + b03 * a32,
+        b00 * a03 + b01 * a13 + b02 * a23 + b03 * a33,
+        b10 * a00 + b11 * a10 + b12 * a20 + b13 * a30,
+        b10 * a01 + b11 * a11 + b12 * a21 + b13 * a31,
+        b10 * a02 + b11 * a12 + b12 * a22 + b13 * a32,
+        b10 * a03 + b11 * a13 + b12 * a23 + b13 * a33,
+        b20 * a00 + b21 * a10 + b22 * a20 + b23 * a30,
+        b20 * a01 + b21 * a11 + b22 * a21 + b23 * a31,
+        b20 * a02 + b21 * a12 + b22 * a22 + b23 * a32,
+        b20 * a03 + b21 * a13 + b22 * a23 + b23 * a33,
+        b30 * a00 + b31 * a10 + b32 * a20 + b33 * a30,
+        b30 * a01 + b31 * a11 + b32 * a21 + b33 * a31,
+        b30 * a02 + b31 * a12 + b32 * a22 + b33 * a32,
+        b30 * a03 + b31 * a13 + b32 * a23 + b33 * a33,
+      ];
+    },
+  
+    translation: function(tx, ty, tz) {
+      return [
+         1,  0,  0,  0,
+         0,  1,  0,  0,
+         0,  0,  1,  0,
+         tx, ty, tz, 1,
+      ];
+    },
+  
+    xRotation: function(angleInRadians) {
+      var c = Math.cos(angleInRadians);
+      var s = Math.sin(angleInRadians);
+  
+      return [
+        1, 0, 0, 0,
+        0, c, s, 0,
+        0, -s, c, 0,
+        0, 0, 0, 1,
+      ];
+    },
+  
+    yRotation: function(angleInRadians) {
+      var c = Math.cos(angleInRadians);
+      var s = Math.sin(angleInRadians);
+  
+      return [
+        c, 0, -s, 0,
+        0, 1, 0, 0,
+        s, 0, c, 0,
+        0, 0, 0, 1,
+      ];
+    },
+  
+    zRotation: function(angleInRadians) {
+      var c = Math.cos(angleInRadians);
+      var s = Math.sin(angleInRadians);
+  
+      return [
+         c, s, 0, 0,
+        -s, c, 0, 0,
+         0, 0, 1, 0,
+         0, 0, 0, 1,
+      ];
+    },
+  
+    scaling: function(sx, sy, sz) {
+      return [
+        sx, 0,  0,  0,
+        0, sy,  0,  0,
+        0,  0, sz,  0,
+        0,  0,  0,  1,
+      ];
+    },
+  
+    translate: function(m, tx, ty, tz) {
+      return m4.multiply(m, m4.translation(tx, ty, tz));
+    },
+  
+    xRotate: function(m, angleInRadians) {
+      return m4.multiply(m, m4.xRotation(angleInRadians));
+    },
+  
+    yRotate: function(m, angleInRadians) {
+      return m4.multiply(m, m4.yRotation(angleInRadians));
+    },
+  
+    zRotate: function(m, angleInRadians) {
+      return m4.multiply(m, m4.zRotation(angleInRadians));
+    },
+  
+    scale: function(m, sx, sy, sz) {
+      return m4.multiply(m, m4.scaling(sx, sy, sz));
+    },
+  
+  };
